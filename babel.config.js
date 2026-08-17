2e5f52d5fd4a8debb0ebab7e63aa4867f7037ee1
Mahmoud Shokry <msn4eg@gmail.com>
2026-08-17 13:34:28 +0300

    init rn 87

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,7 @@
 module.exports = {
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@': './src',
-        },
-        extensions: ['.js', '.json'],
-        root: ['./src'],
-      },
-    ],
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    'inline-dotenv',
-    '@babel/plugin-proposal-export-namespace-from',
-    'react-native-worklets/plugin',
-  ],
   presets: ['module:@react-native/babel-preset'],
-  "env": {
-    "development": {
-      "compact": false
-    },
-    "production": {
-      "plugins": [
-        [
-          "transform-remove-console",
-          {
-            "exclude": [
-              "error",
-              "warn"
-            ]
-          }
-        ]
-      ]
-    }
-  },
+  plugins: [
+    'transform-inline-environment-variables',
+     'react-native-worklets/plugin',
+  ],
 };
